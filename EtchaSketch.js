92c67de4c22b16940e9eb71eaf6b50a09abc10a6
Wander5 <wja14msu@gmail.com>
2023-06-28 05:41:23 -0500

    create skeleton and input field for grid size

---
+++ EtchaSketch.js
@@ -0,0 +1,11 @@
+const div = document.createElement("div");
+
+const gridDimensions = document.getElementById("grid-dimensions");
+
+document.getElementById("container").appendChild(div);
+
+function getGridDimension(num) {
+    // input, ask user for number
+    gridDimensions = gridDimensions.value;
+
+}
