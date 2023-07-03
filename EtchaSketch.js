ed128b4e0632e585cffb34955771cc5dda81d4c0
Wander5 <wja14msu@gmail.com>
2023-07-03 06:00:45 -0500

    add practice div/pixel on hover

--- EtchaSketch.js
+++ EtchaSketch.js
@@ -1,6 +1,7 @@
 const div = document.createElement("div");
 
 const gridDimensions = document.getElementById("grid-dimensions");
+const container = document.getElementById("container");
 
 const form = document.getElementById('form');
 form.addEventListener('submit', function(event) {
@@ -14,6 +15,7 @@ const btn = document.getElementById('submit');
 
 form.addEventListener('click', function() {
     document.getElementById("dimension-input").value;
+    
 })
 
 //document.getElementById("container").appendChild(div);
@@ -22,10 +24,19 @@ document.body.onload = addElement;
 function addElement() {
     const newDiv = document.createElement("div");
 
-    const newContent = document.createTextNode("This is text", dimensions);
+    const newContent = document.createTextNode("This is text", gridDimensions);
 
     newDiv.appendChild(newContent);
 
     const currentDiv = document.getElementById("input-container");
     document.body.insertBefore(newDiv, currentDiv);
 }
+
+function addDiv() {
+    let numDivs = document.getElementById("grid-dimensions").value;
+    for (i = 0; i <= numDivs; i++) {
+        let newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+    }
+}
+
