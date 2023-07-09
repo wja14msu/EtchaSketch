0ec3c33c310c4b18bbf54782fa02a6dfda83d0e3
Wander5 <wja14msu@gmail.com>
2023-07-09 06:00:05 -0500

    more grid work. trying to figure out how to make the number of boxes dynamic while keeping them inside the grid container.

--- EtchaSketch.js
+++ EtchaSketch.js
@@ -1,24 +1,27 @@
 const gridContainer = document.getElementById("grid-container");
-let gridSize = 24;
+let gridSize = 10;
+//let gridSize = document.getElementById("grid-size").value;
 
 
 function createGrid() {
+    //gridContainer.innerHTML = "";
+
     let gridWidth = gridContainer.offsetWidth / gridSize;
-    //let gridSize = document.getElementById("grid-size").value;
+    gridSize = document.getElementById("grid-size").value;
 
     // Clear previous grid
-    gridContainer.innerHTML = "";
+    //gridContainer.innerHTML = "";
 
     // Set grid template columns based on grid sizee
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize - 3}, ${gridWidth}px) 1fr 1fr 1fr`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridSize - 3}, ${gridWidth}px) 1fr 1fr 1fr`;
+    //gridContainer.style.gridTemplateRows = `repeat(${gridSize - 3}, ${gridWidth}px) 1fr 1fr 1fr`;
     }
 
     // Create grid items
-    for (let i = 0; i < gridSize ** 2; i++) {
+    for (let i = 0; i <= gridSize ** 2; i++) {
         const square = document.createElement("div")
         square.classList.add("grid-item");
         square.setAttribute('draggable', 'false');
+        square.textContent = "box" + `${i}`;
         gridContainer.appendChild(square);
     }
-}
